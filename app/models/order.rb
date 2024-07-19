5ae8053fd37b7c9e1f94c901f4a295bdabbd74f4
Knight~ <x665adex563@gmail.com>
2024-07-19 12:09:42 +0800

    完成 將網址改為秘密

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :user
+  before_create :generate_token
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
